0d4e34aa7ff96e574f7d78cb00e7c583ecb7e4fd
Tejas <62064506+KidCoderT@users.noreply.github.com>
2022-05-12 17:54:41 +0530

    Basic Html File

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "<p>My app</p>"
+    return render_template("index.html")
 
 app.run(debug=True)
